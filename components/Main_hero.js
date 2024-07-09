63b6417e0b4e8cdc6827846ba4a529d98498bc62
Ripon-Mardy <mardyripon10@gmail.com>
2024-07-09 22:01:36 +0600

    change

--- components/Main_hero.js
+++ components/Main_hero.js
@@ -1,5 +1,6 @@
+'use client'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 
 
@@ -17,6 +18,125 @@ import choose1 from './../public/image/why choose us/images.jpg'
 
 
 const Main_hero = () => {
+
+    const [isShowLessNav, setShowLessNav] = useState(false);
+
+
+    const linkTexts = [
+        {
+            "linkText": "adhesives & Sealants",
+            "href": "#"
+        },
+        {
+            "linkText": "ATEX",
+            "href": "#"
+        },
+        {
+            "linkText": "Automation & Control Components",
+            "href": "#"
+        },
+        {
+            "linkText": "Batteries",
+            "href": "#"
+        },
+        {
+            "linkText": "Cable Accessories",
+            "href": "#"
+        },
+        {
+            "linkText": "Circuit Protection",
+            "href": "#"
+        },
+        {
+            "linkText": "Clean Room",
+            "href": "#"
+        },
+        {
+            "linkText": "Computing & Peripherals",
+            "href": "#"
+        },
+        {
+            "linkText": "Displays, Lighting & Optoelectronics",
+            "href": "#"
+        },
+        {
+            "linkText": "Electrical Cables & Wires",
+            "href": "#"
+        },
+        {
+            "linkText": "Electrical Conectors & Cables",
+            "href": "#"
+        },
+        {
+            "linkText": "Electrical Test & Measurement Equipment",
+            "href": "#"
+        },
+        {
+            "linkText": "Enclosures, Storage & Material Handling",
+            "href": "#"
+        },
+        {
+            "linkText": "HVAC, Fans & Thermal Management",
+            "href": "#"
+        },
+        {
+            "linkText": "Network, Power & Signal Connectors",
+            "href": "#"
+        },
+        {
+            "linkText": "Panel Meters & Accessories",
+            "href": "#"
+        },
+        {
+            "linkText": "Passive Components, Crystals & Oscillators",
+            "href": "#"
+        },
+        {
+            "linkText": "Pneumatics, Hydraulics & Power Transmission",
+            "href": "#"
+        },
+        {
+            "linkText": "Power Supplies",
+            "href": "#"
+        },
+        {
+            "linkText": "Fire Alarms, Safety Control & Security",
+            "href": "#"
+        },
+        {
+            "linkText": "Semiconductor Devices",
+            "href": "#"
+        },
+        {
+            "linkText": "Sensors, Encoders & Transducers",
+            "href": "#"
+        },
+        {
+            "linkText": "Hand Tools & Supplies Computer Engineering Software & Licences",
+            "href": "#"
+        },
+        {
+            "linkText": "PPE - Personal Protective Equipment",
+            "href": "#"
+        },
+        {
+            "linkText": "Adhesives & Sealants",
+            "href": "#"
+        },
+        {
+            "linkText": "Adhesives & Sealants",
+            "href": "#"
+        },
+
+    ]
+
+    const displayedLink = isShowLessNav ? linkTexts : linkTexts.slice(0, 10);
+
+    const handleshowClick = () => {
+        setShowLessNav(!isShowLessNav);
+    }
+
+
     return (
         <div className='container mx-auto px-3 md:px-0 py-10'>
 
@@ -26,68 +146,52 @@ const Main_hero = () => {
 
                 {/* ==== left side bar ===  */}
                 <div className=' xl:w-1/4'>
-                   <div className='flex flex-col gap-8'>
-                   <div className='border-2 border-navBorder rounded-md'>
-                        <h1 className='bg-navBgColor text-white py-4 pl-3 text-xl capitalize font-medium'>categories</h1>
+                    <div className='flex flex-col gap-8'>
+                        <div className='border-2 border-navBorder rounded-md'>
+                            <h1 className='bg-navBgColor text-white py-2 pl-3 text-xl capitalize font-medium'>categories</h1>
 
-                        <div className='flex flex-col gap-4 p-3 text-textNavColor font-semibold text-sm capitalize'>
-                            <Link href={'#'}>adhesives & Sealants</Link>
-                            <Link href={'#'}>ATEX</Link>
-                            <Link href={'#'}>Automation & Control Components</Link>
-                            <Link href={'#'}>Batteries</Link>
-                            <Link href={'#'}>Cable Accessories</Link>
-                            <Link href={'#'}>Circuit protection</Link>
-                            <Link href={'#'}>clean room</Link>
-                            <Link href={'#'}>Computing & Peripherals</Link>
-                            <Link href={'#'}>Displays, Lighting & Optoelectronics</Link>
-                            <Link href={'#'}>Electrical Cables & Wires</Link>
-                            <Link href={'#'}>Electrical Conectors & Cables</Link>
-                            <Link href={'#'}>Electrical Test & Measurement Equipment</Link>
-                            <Link href={'#'}>Enclosures, Storage & Material Handling</Link>
-                            <Link href={'#'}>HVAC, Fans & Thermal Management</Link>
-                            <Link href={'#'}>Network, Power & signal Connectors</Link>
-                            <Link href={'#'}>Panel Meters & Accessories</Link>
-                            <Link href={'#'}>Passive Components, Crystals & Oscillators</Link>
-                            <Link href={'#'}>Pneumatics, Hydraulics & Power Transmission</Link>
-                            <Link href={'#'}>Power Supplies</Link>
-                            <Link href={'#'}>Fire Alarms, Safety Control & Security</Link>
-                            <Link href={'#'}>Semiconductor Devices</Link>
-                            <Link href={'#'}>Sensors, Encoders & Transducers</Link>
-                            <Link href={'#'}>Hand Tools & Supplies Computer Engineering Software & Licences</Link>
-                            <Link href={'#'}>Power Quality & Control</Link>
-                            <Link href={'#'}>PPE - Personal Protective Equipment</Link>
+                            <div className='flex flex-col gap-4 p-3 text-textNavColor font-semibold text-sm capitalize'>
+                                {
+                                    displayedLink.map((linkText, linkIndex) => (
+                                        <div key={linkIndex}>
+                                            <Link href={linkText.href} >{linkText.linkText} </Link>
+                                        </div>
+                                    ))
+                                }
+                            </div>
+
+                            <button onClick={handleshowClick} className='bg-navBgColor text-white text-base py-0.5 capitalize font-medium block w-full'> {isShowLessNav ? 'See Less' : 'See More'} </button>
                         </div>
+
+                        {/* ==== left side bannar ====  */}
+                        <div className='hidden md:block'>
+                            <Image src={productBanner} className='rounded-md' />
+                        </div>
+                        {/* ==== end left side banner ====  */}
+
+
+                        {/* ====why choose us ===  */}
+                        <div className='hidden md:block'>
+                            <h1 className='text-xl font-semibold text-center mb-2'>Why Choose us?</h1>
+                            <Image src={choose1} className='rounded-md' />
+                        </div>
+                        {/* ===== end why choose us ====  */}
+
+
+                        {/* ==== left side bannar ====  */}
+                        <div className='hidden md:block'>
+                            <Image src={productBanner} className='rounded-md' />
+                        </div>
+                        {/* ==== end left side banner ====  */}
+
+
+
+
                     </div>
-
-                    {/* ==== left side bannar ====  */}
-                    <div className='hidden md:block'>
-                        <Image src={productBanner} className='rounded-md' />
-                    </div>
-                    {/* ==== end left side banner ====  */}
-
-
-                    {/* ====why choose us ===  */}
-                    <div className='hidden md:block'>
-                        <h1 className='text-xl font-semibold text-center mb-2'>Why Choose us?</h1>
-                        <Image src={choose1} className='rounded-md' />
-                    </div>
-                    {/* ===== end why choose us ====  */}
-
-
-                     {/* ==== left side bannar ====  */}
-                     <div className='hidden md:block'>
-                        <Image src={productBanner} className='rounded-md' />
-                    </div>
-                    {/* ==== end left side banner ====  */}
-
-
-
-
-                   </div>
 
 
                 </div>
-                {/* === end side bar ===  */}
+                {/* === end left side bar ===  */}
 
 
 
@@ -121,14 +225,14 @@ const Main_hero = () => {
 
                     {/* ==== proud clients ====  */}
                     <div>
-                        <Proud_clients/>
+                        <Proud_clients />
                     </div>
                     {/* ====end proud clients ====  */}
 
 
                     {/* ==== servcies ===  */}
                     <div className='my-10'>
-                        <Services/>
+                        <Services />
                     </div>
                     {/* ==== end services ===  */}
 
