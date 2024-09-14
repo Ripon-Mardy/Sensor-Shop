dc8b4d93b3701f58545cf859836a66df029de683
Ripon-Mardy <mardyripon10@gmail.com>
2024-09-14 13:34:52 +0600

    change

--- app/products/[slug]/page.js
+++ app/products/[slug]/page.js
@@ -6,26 +6,22 @@ import Image from "next/image";
 import Related_product from "@/components/Related_product";
 import Loading from "@/components/Loading";
 import Get_a_quote from "@/components/Get_a_quote";
+import { SlSizeFullscreen } from "react-icons/sl";
+import { IoIosClose } from "react-icons/io";
 
 const page = ({ params }) => {
   const slug = params.slug;
 
-  const [product, setProduct] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
+  const [product, setProduct] = useState([]); // set product data
+  console.log('product', product);
+  
+  const [loading, setLoading] = useState(true); // set loading 
+  const [error, setError] = useState(false); // set error
   const [productExtraFieldIndex1, setProductExtraField1] = useState([]);
   const [productExtraFieldIndex2, setProductExtraField2] = useState([]);
-
-  const [productImages, setProudctImages] = useState([]);
-
-  const [mainImage, setMainImage] = useState(product);
-
-  // const [mainImage, setMainImage] = useState(images[0]);
-  const [isZoomed, setIsZoomed] = useState(false);
-  const [zoomPosition, setZoomPosition] = useState({ x: "50%", y: "50%" });
-  const imageContainerRef = useRef(null);
-
-  const [isFormVisible, setIsFormVisible] = useState(false);
+  const [productImage, setProductImage] = useState(); // product images add
+  const [isFullScreen, setIsFullScreen] = useState(false); // Fullscreen state
+  const [isFormVisible, setIsFormVisible] = useState(false); // form visible
 
   const openPopUp = () => {
     setIsFormVisible(!isFormVisible);
@@ -46,10 +42,7 @@ const page = ({ params }) => {
         }
         const data = await res.json();
         setProduct(data.data);
-        setProductExtraField1(data.data.extra_fields[0]);
-        setProductExtraField2(data.data.extra_fields[1]);
-        setProudctImages(data.data.extra_fields[0].meta_value);
-        setMainImage(data.data.extra_fields[0].meta_value[0]);
+        setProductImage(data.data.featured_image);
       } catch (error) {
         setError("Error", error.message);
       } finally {
@@ -71,24 +64,8 @@ const page = ({ params }) => {
     return <div> {error} </div>;
   }
 
-  const handleMouseMove = (e) => {
-    const { left, top, width, height } =
-      imageContainerRef.current.getBoundingClientRect();
-    const x = ((e.pageX - left) / width) * 100;
-    const y = ((e.pageY - top) / height) * 100;
-    setZoomPosition({ x: `${x}%`, y: `${y}%` });
-  };
-
-  const handleMouseEnter = () => {
-    setIsZoomed(true);
-  };
-  const handleMouseLeave = () => {
-    setIsZoomed(false);
-  };
-
-  const handleImageClick = (image) => {
-    setMainImage(image);
-  };
+  const openFullScreen = () => setIsFullScreen(true);
+  const closeFullScreen = () => setIsFullScreen(false);
 
   return (
     <>
@@ -102,84 +79,92 @@ const page = ({ params }) => {
           </div> */}
           {/* ===== product details ====  */}
           <div className="flex flex-col md:flex-row gap-10 md:gap-20 py-10">
-            <div className="md:basis-1/2 border border-gray-200 rounded-md">
-              <Image
+            <div className="md:basis-1/2 border border-gray-200 rounded-md flex flex-col gap-5 p-5">
+              {/* <Image
                 src={product.featured_image}
                 className=" mx-auto w-3/4 md:w-2/3"
                 width={200}
                 height={200}
-              />
+              /> */}
 
-              {/* ==== product slider ====  */}
-              {/* <div className="flex flex-col items-center">
-                  <div
-                    className="w-3/4 h-full mb-3 overflow-hidden relative"
-                    onMouseMove={handleMouseMove}
-                    onMouseEnter={handleMouseEnter}
-                    onMouseLeave={handleMouseLeave}
-                    ref={imageContainerRef}
+              <div className="md:w-3/4 md:h-1/2 mx-auto">
+                {/* ==== product slider ====  */}
+                <div className="relative border border-gray-200">
+                  <Image
+                    src={productImage}
+                    width={200}
+                    height={200}
+                    alt={product.name}
+                    className="w-full object-cover mx-auto rounded-lg"
+                    layout="responsive"
+                  />
+                  <span onClick={openFullScreen} className="absolute right-3 bottom-3 text-xl border border-gray-300 p-1 cursor-pointer rounded-md text-white bg-gray-600">
+                    <SlSizeFullscreen />
+                  </span>
+                </div>
+                {/* ==== full screen image ====  */}
+                {/* Full Screen Modal */}
+            {isFullScreen && (
+              <div className="fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center z-50">
+                <div className="relative">
+                  <img
+                    src={productImage}
+                    alt="Full Screen"
+                    className="w-full h-full object-cover"
+                  />
+                  <button
+                    className="absolute top-4 right-4 bg-gray-800 text-white p-2 rounded-full"
+                    onClick={closeFullScreen}
                   >
-                    <Image
-                      src={mainImage}
-                      width={200}
-                      height={200}
-                      alt="Main Product"
-                      className={`w-full h-full object-cover transition-transform duration-300 ${
-                        isZoomed ? "md:scale-150" : ""
-                      } `}
-                    />
-                  </div>
-
-                  <div className="flex space-x-4">
-                    { productImages.length === 0 && productImages.map((image, index) => (
+                    <IoIosClose/>
+                  </button>
+                </div>
+              </div>
+            )}
+                {/* ==== end full screen images ====  */}
+                <div className="flex items-center justify-center gap-4 mt-5">
+                  {product.extra_fields[0]?.meta_value.map((img) => (
+                    <div className="border border-gray-200">
                       <Image
-                        key={index}
-                        src={image}
+                        onClick={() => setProductImage(img)}
+                        src={img}
                         width={100}
                         height={100}
-                        alt={`Product ${index + 1}`}
-                        onClick={() => handleImageClick(image)}
-                        className={`w-16 h-16 object-cover cursor-pointer transform transition-transform duration-300 hover:scale-105 ${
-                          image === mainImage
-                            ? "border-2 border-blue-500"
-                            : "border border-gray-300"
-                        }`}
+                        className="cursor-pointer rounded-md"
                       />
-                    ))}
-                  </div>
-                </div> */}
-              {/* === end product slider ===  */}
-
-
+                    </div>
+                  ))}
+                </div>
+                {/* === end product slider ===  */}
+              </div>
             </div>
             {/* ===right ==  */}
             <div className="md:basis-1/2 flex flex-col gap-5 md:gap-5">
               <h1 className="text-2xl md:text-3xl text-header_text font-bold">
                 {product.name}
               </h1>
-              <p className="text-para_color">
-                {productExtraFieldIndex1.meta_value}
-              </p>
-              <h2 className="text-xl text-header_text font-semibold">
-                Regular Price : {productExtraFieldIndex2.meta_value} tk
-              </h2>
-              <div>
-                <div className="text-para_color font-medium mt-1 md:mt-0 flex flex-col gap-2 text-sm text-left">
-                  <h1 className="flex gap-5">
-                    Product Code : <span> {product.meta_title} </span>
-                  </h1>
-                  <h1 className="flex gap-5">
-                    Delivery Area : <span> {product.meta_description} </span>
-                  </h1>
-                  <h1 className="flex gap-5">
-                    Condition : <span> New Factory Sealed</span>
-                  </h1>
-                  <h1 className="flex gap-5">
-                    From : <span> {product.meta_keyword} </span>
-                  </h1>
-                  <h1 className="flex gap-5">
-                    Warranty :<span> {product.meta_author} </span>
-                  </h1>
+              <p> {product.meta_description} </p>
+
+              <div className="flex flex-col gap-3"> 
+                {/* <div className="flex items-start gap-7">
+                  <h1 className="font-medium w-20">Brand</h1>
+                  <span> {product.extra_fields[1].term_type} </span>
+                </div> */}
+                <div className="flex items-start gap-4 text-lg">
+                  <h1 className="font-medium">Origin</h1>
+                  <span> {product.term_type} </span>
+                </div>
+                <div className="flex items-start gap-4">
+                  <h1 className="font-medium">Condition</h1>
+                  <span> {product.meta_title} </span>
+                </div>
+                <div className="flex items-start gap-4">
+                  <h1 className="font-medium">warranty</h1>
+                  <span> {product.term_type} </span>
+                </div>
+                <div className="flex items-start gap-4">
+                  <h1 className="font-medium">Price</h1>
+                  <span className="text-lg font-semibold"> {product.extra_fields[1].meta_value} tk </span>
                 </div>
               </div>
               <button
